165f808c96958bcd82190bbbfe5fd52de53ceb65
FIRST_NAME LAST_NAME <mlubin@enoahisolution.com>
2023-07-23 19:52:30 +0530

    fix

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -49,7 +49,7 @@
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Description') }}</label>
                             <div class="col-md-6">
-                                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="exampleFormControlTextarea1" rows="3">{{ $book->author ?? old('author') }}</textarea>
+                                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="exampleFormControlTextarea1" rows="3">{{ $book->description ?? old('description') }}</textarea>
                                 @error('description')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
